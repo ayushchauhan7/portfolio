a51c9ea18330b0ce4324eb1cb87e332d5fc43eb1
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-06-01 13:24:38 +0530

    second

--- src/main/java/in/ayushchauhan/portfolio/controller/HomeController.java
+++ src/main/java/in/ayushchauhan/portfolio/controller/HomeController.java
@@ -1,4 +1,13 @@
 package in.ayushchauhan.portfolio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping({"", "/", "/home"})
+    public String showHomePage() {
+return "home";
+}
 }
