38f5c7536e3977e4fbe0077debbbb7034e72d709
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-06-02 21:55:27 +0530

    third

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.querySelector('.loader-wrapper').style.opacity = '0';
+        setTimeout(() => {
+            document.querySelector('.loader-wrapper').style.opacity = 'none';
+        }, 500)
+    }, 2000)
+});
